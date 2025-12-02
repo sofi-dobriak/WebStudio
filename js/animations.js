68a6915402c1cac7860ba27243dcd012afb55532
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-12-02 17:32:20 +0200

    feat: add animations

---
+++ js/animations.js
@@ -0,0 +1,42 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const sectionTitles = document.querySelectorAll('.section-anim-title');
+
+sectionTitles.forEach(titleElement => {
+  const sectionTitleSplit = new SplitType(titleElement, { types: 'chars' });
+
+  gsap.from(sectionTitleSplit.chars, {
+    opacity: 0,
+    stagger: 0.04,
+    duration: 0.08,
+    scrollTrigger: {
+      trigger: titleElement,
+      start: 'top 50%',
+      markers: true,
+    },
+  });
+});
+
+function scrollAnimation(targetSelector, triggerSelector) {
+  gsap.fromTo(
+    targetSelector,
+    {
+      y: 250,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: triggerSelector,
+        start: 'top 50%',
+      },
+    }
+  );
+}
+
+scrollAnimation('.advantages-item', '.advantages-section');
+scrollAnimation('.team-item', '.team-section');
+scrollAnimation('.portfolio-item', '.portfolio-section');
